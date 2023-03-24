bd15154da3c4a5bf55bbfd41a05f95e4ab00917e
xylaron <aldo6701@gmail.com>
2023-03-24 23:37:25 +0800

    added API managing orders

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,13 +1,12 @@
 import { createTRPCRouter } from "server/api/trpc";
-import { exampleRouter } from "server/api/routers/example";
-
+import { ordersRouter } from "./routers/orders";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  orders: ordersRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/orders.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "server/api/trpc";
+
+export const ordersRouter = createTRPCRouter({
+  create: publicProcedure
+    .input(
+      z.object({
+        orderData: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.orders.create({
+        data: {
+          orderData: input,
+        },
+      });
+    }),
+});
